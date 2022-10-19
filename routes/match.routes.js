38f5ebcf792a655c15af59d84ef342e964994d74
Yabel <yabelrr@gmail.com>
2022-10-19 19:53:23 +0200

    Working with new schema and updating likes array

--- routes/match.routes.js
+++ routes/match.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User.model');
+const ObjectId = require('mongodb').ObjectId;
 
 // //MATCH PAGE
 router.get("/", /*isLogged,*/ (req, res, next) => {
@@ -14,18 +15,55 @@ router.get("/", /*isLogged,*/ (req, res, next) => {
   })
   });
 router.post("/", /*isLogged,*/ (req, res, next) => {
-    User.findById(req.session.currentUser._id)
-    .then(result => {
-      const logedUser = result;
-      console.log('BODYYY----------------->', req.body)
-      // logedUser.likes.push(req.body)
-      console.log('whats is this ----------------->', logedUser.likes)
+    if(req.body.yes){ 
+     
+
+      console.log('aaaaa-------', req.session.currentUser.likes)
       
-      res.render('match/match', logedUser.username)
-    })
-    .catch(err => {
-      console.log(err)
-    })
-   });
+     
+      req.session.currentUser.likes.forEach(element => {
+        console.log('ELEMENT --------> ', typeof element)
+        if(!id.equals(element)){
+          return res.redirect('/match')
+        }
+      })
+
+      // console.log(typeof req.body.yes)
+      // if(req.session.currentUser.likes.includes(req.body.yes)){
+      //   return res.redirect('/match')
+      // }
+      
+      // const strLikes = JSON.stringify(req.session.currentUser.likes);
+      // // console.log(strLikes, typeof strLikes)
+      // const strId = JSON.stringify(req.body.yes);
+
+      // if(strLikes.includes(strId)){
+      //   return res.redirect('/match')
+      // }
+
+
+      const id = ObjectId(req.body.yes);
+    
+
+        User.findByIdAndUpdate(req.session.currentUser._id, { "$push": { "likes": req.body.yes } })
+        .then(result => {
+          res.redirect('/match')
+        })
+        .catch(err => {
+          console.log(err)
+        })
+
+    } else {
+      User.findByIdAndUpdate(req.session.currentUser._id, { "$push": { "dislikes": req.body.no } })
+      .then(result => {
+        res.redirect('/match')
+      })
+      .catch(err => {
+        console.log(err)
+      })
+
+    }
+
+    });
 
 module.exports = router;
